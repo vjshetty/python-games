a309a84141ace8ca5a90e63784abcec1e497e57e
Vijeth Shetty <vijethshetty@Vijeths-Laptop.local>
2024-09-04 11:04:26 -0700

    Added ger choices() function and the condition

--- rock-papers-siccors.py
+++ rock-papers-siccors.py
@@ -7,5 +7,25 @@ def get_choices():
         choices = {"player": player_choice, "computer": computer_choice}
         return choices
 
-choices = get_choices()
-print (choices)
+def check_win(player,computer):
+        print(f"You chose {player}, computer chose {computer}")
+        if player == computer:
+                return "its a tie!"
+        elif player =="rock":
+            if computer =="scissors":
+                return "Rock smashes scissors ! You win!"
+            else:
+                return "Paper Covers rock ! You lose."
+        elif player =="paper":
+            if computer =="rock":
+                return "Paper Covers rock ! You win!"
+            else:
+                return "Scissors cut paper ! You lose."
+        elif player =="Scissors":
+            if computer =="paper":
+                return "Scissors cut paper ! You win!"
+            else:
+                return "Rock smashes scissors ! You lose."
+choices =get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
